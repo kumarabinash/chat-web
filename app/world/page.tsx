f428144f659b90232265c0d63b6e41f1b63520c9
Kumar Abinash <abi.cd.sh@gmail.com>
2025-02-25 10:44:15 +0100

    Basic socket connection and sending & receiveing message

--- app/world/page.tsx
+++ app/world/page.tsx
@@ -1,28 +1,81 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { Socket, io } from 'socket.io-client';
+import { CHAT_EVENTS, USER_EVENTS } from '@/types/event.enum';
+
+const socket: Socket = io('http://localhost:3001');
+
+interface Message {
+  message: string;
+  sender: string;
+  timestamp: string;
+}
 
 export default function WorldChat() {
   const router = useRouter();
-
   const userName = sessionStorage.getItem('username');
+  const [isConnected, setIsConnected] = useState(false);
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+      setIsConnected(true);
+    });
+
+    socket.on('connect_error', (error) => {
+      console.error('Connection error:', error);
+      setIsConnected(false);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+      setIsConnected(false);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('connect_error');
+      socket.off('disconnect');
+    };
+  }, []);
 
   useEffect(() => {
     if (!userName) {
       router.replace('/');
+      return;
     }
-  }, [router, userName]);
+
+    if (isConnected) {
+      socket.emit(USER_EVENTS.USER_JOIN_REQUEST, userName);
+    }
+  }, [router, userName, isConnected]);
 
   const handleLogout = () => {
     sessionStorage.clear();
     router.replace('/');
   };
 
+  useEffect(() => {
+    socket.on(USER_EVENTS.USER_JOIN_REQUEST, (message: string) => {
+      console.log(message);
+    });
+
+    socket.on(CHAT_EVENTS.CHAT_MESSAGE_RECEIVED, (message: Message) => {
+      console.log(message);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, [socket]);
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const message = form.message.value;
+
+    socket.emit(CHAT_EVENTS.CHAT_MESSAGE, message);
 
     form.reset();
     console.log(message);
@@ -44,11 +97,11 @@ export default function WorldChat() {
       <div className="flex flex-col h-[calc(100vh-100px)] border border-gray-300 rounded-md p-4">
         <div className="flex-1 overflow-y-auto">
           <div className="flex flex-col gap-2">
-            <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-500">John Doe</span>
-              <span className="text-xs text-gray-500">12:00 PM</span>
-            </div>
-            <p className="text-sm">Hello, how are you?</p>
+            {messages.map((message, index) => (
+              <div key={index} className="flex justify-between items-center">
+                <span className="text-sm text-gray-500">{message.message}</span>
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex-none">
